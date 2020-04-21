358edd0591c65fa2b6490709e0b3c86a4e91dc5b
Nazanin <nazaninpolite@gmail.com>
2020-04-21 00:21:16 -0700

    make cards

--- src/components/AdvSearch/bookCards/bookCard.js
+++ src/components/AdvSearch/bookCards/bookCard.js
@@ -1,14 +1,59 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { CardMedia } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+      width: '50%',
+      margin: 'auto',
+      padding: '1%',
+      height: '50vh',
+      marginTop: '5vh',
+    },
+    media: {
+        height: 140,
+      },
+  });
 
 function BookCard(props) {
-    console.log(props);
+    const classes = useStyles();
+
     return(
         <div>
-            <img src={props.book.imgUrl}/>
-            <h4>Title: {props.book.title}</h4>
-            <p>Author: {props.book.author}</p>
-            <p>Publisher: {props.book.publisher}</p>
-            <button>Owner Contact</button>
+            <Card className={classes.root}>
+                <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image="/img/book1.jpg"
+                    title="Book Cover"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Author: {props.book.author}
+                    </Typography>
+                    <h4>
+                    Title:
+                    {props.book.title}
+                    </h4>
+                    <h4>
+                    Publisher:
+                    {props.book.publisher}
+                    </h4>
+                </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Contact Info
+                    </Button>
+                </CardActions>
+            </Card>
         </div>
     );
 }
